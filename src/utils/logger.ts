17dfde7501e576ffb81a2b08d5a3839ac7551d47
Delta.v <tmm.michalski@gmail.com>
2022-07-21 15:45:48 +0200

    configure logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,15 @@
-import logger from 'pino';
+import pino from 'pino';
 import dayjs from 'dayjs';
 
-const log = logger({
-    prettyPrint: true,
-    base: {
-        pid: false,
+const logger = pino({
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            colorize: true,
+            ignore: 'pid,hostname',
+            translateTime: 'dd/mm/yyyy HH:MM:ss',
+        },
     },
-    timestamp: () => `,"time":"${dayjs().format}"`,
 });
 
-export default log;
+export default logger;
